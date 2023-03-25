96e7759e82c54cbcca954751074d3674ae63b595
Guilherme A <68473916+Guilherme-A-Silva@users.noreply.github.com>
2023-03-25 10:32:13 -0300

    --

--- ControleDeposito.Api/src/main/resources/db/migration/V001__Criar-Tabela-Produtos.sql
+++
@@ -1,9 +0,0 @@
-create table Produto(
-	Codigo bigint not null auto_increment,
-	Nome varchar(60) not null,
-	Descricao varchar(255) not null,
-	Quantidade bigint not null,
-	Preco double not null,
-	
-	primary key(Codigo)
-);
