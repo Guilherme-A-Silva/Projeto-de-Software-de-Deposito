49f3401a10e413421190011f8d50ff7c496d4730
Guilherme A <68473916+Guilherme-A-Silva@users.noreply.github.com>
2023-03-28 11:43:28 -0300

    --

--- ControleDeposito.Api/src/main/java/com/ControleDeposito/domain/model/StatusEncomenda.java
+++
@@ -1,6 +0,0 @@
-package com.ControleDeposito.domain.model;
-
-public enum StatusEncomenda {
-	
-	PENDENTE, FINALIZADA, CANCELADA
-}
--- ControleDeposito.Api/src/main/resources/db/migration/V002__Criar-Tabela-Encomendas.sql
+++
@@ -1,9 +0,0 @@
-create table Encomenda(
-	Codigo bigint not null auto_increment,
-	Nome varchar(60) not null,
-	Descricao varchar(255) not null,
-	Quantidade bigint not null,
-	Status varchar(20) not null,
-	
-	primary key(Codigo)
-);
